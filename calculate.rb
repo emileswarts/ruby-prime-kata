918649d98134bd4e811d2dc821b54512c279b96e
Emile Swarts <emile.swarts123@gmail.com>
2013-03-29 08:43:01 +0000

    refactor

--- calculate.rb
+++ calculate.rb
@@ -1,8 +1,12 @@
 require './prime_numbers.rb'
 require 'pp'
 
-@prime_count = 10
+if ARGV.empty?
+		@prime_count = 10
+else
+		@prime_count = ARGV.shift.to_i
+end
 
-@primes = Primefactors.new
+@primes = Primefactors.new.get_primes(@prime_count)
 @renderer = Renderer.new
-@renderer.render_table(@primes.get_primes(@prime_count) )
+@renderer.render_table(@primes )
